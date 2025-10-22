709f4a5c69de52b1cacf0a98c64b6fbda61a3e2e
hoza2000 <zdenek.honza@email.cz>
2025-10-23 02:52:01 +0300

    updated argparser with args for testing

---
+++ general/arg.py
@@ -0,0 +1,56 @@
+import argparse
+
+
+def parse_train_args():
+    ap = argparse.ArgumentParser(description="Train MLP + Cosine classifier")
+
+    ap.add_argument(
+        "--embeddings_root",
+        type=str, required=True,
+        help="Root folder with .npy embeddings"
+    )
+    ap.add_argument(
+        "--outdir",
+        type=str, default="./checkpoints/tmp",  # old files will be lost if path not specified
+        help="Where to save model and all files created during training"
+    )
+    ap.add_argument(
+        "--device",
+        type=str, default="cpu",
+        choices=["cpu", "cuda", "mps"]
+    )
+
+    ap.add_argument("--epochs", type=int, default=15)
+    ap.add_argument("--batch_size", type=int, default=128)
+    ap.add_argument("--hidden_dim", type=int, default=512)
+    ap.add_argument("--dropout", type=float, default=0.3)
+    ap.add_argument("--scale", type=float, default=30.0)
+    ap.add_argument("--lr", type=float, default=1e-3)
+    ap.add_argument("--seed", type=int, default=42)
+
+    return ap.parse_args()
+
+
+def parse_test_args():
+    ap = argparse.ArgumentParser(description="Evaluate trained MLP with MC Dropout")
+
+    ap.add_argument(
+        "--run_dir",
+        type=str, required=True,
+        help="Directory with model.pt, label_map.json, and splits.json"
+    )
+    ap.add_argument(
+        "--mc_passes",
+        type=int, default=30,
+        help="Number of Monte Carlo dropout passes"
+    )
+    ap.add_argument(
+        "--device",
+        type=str, default="cpu",
+        choices=["cpu", "cuda", "mps"]
+    )
+
+    ap.add_argument("--batch_size", type=int, default=128)
+    ap.add_argument("--seed", type=int, default=42)
+
+    return ap.parse_args()
